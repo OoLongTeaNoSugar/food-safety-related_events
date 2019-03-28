d403eab3b46b511dc5ceb091b547e9cd57ec274a
lex1burner <1582980725@qq.com>
2019-03-28 15:07:47 +0800

    updated on 2019-03-28 add readme file add attention+lstm model

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,156 @@
 # -*- ecoding:utf-8 -*-
+import re
+import time
+import gc
+import random
+import os
+
+import numpy as np
+import pandas as pd
 
 # 处理大小写,错漏词,重复词等
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+embed_size = 300 # how big is each word vector
+max_features = 120000 # how many unique words to use (i.e num rows in embedding vector)
+maxlen = 72 # max number of words in a question to use
+
+# 清除非法字符
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…',
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─',
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞',
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
+# 去除数字
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+# 错误拼写修正/缩写扩张，字典可修改（针对数据集）
+mispell_dict = {"aren't" : "are not",
+"can't" : "cannot",
+"couldn't" : "could not",
+"didn't" : "did not",
+"doesn't" : "does not",
+"don't" : "do not",
+"hadn't" : "had not",
+"hasn't" : "has not",
+"haven't" : "have not",
+"he'd" : "he would",
+"he'll" : "he will",
+"he's" : "he is",
+"i'd" : "I would",
+"i'd" : "I had",
+"i'll" : "I will",
+"i'm" : "I am",
+"isn't" : "is not",
+"it's" : "it is",
+"it'll":"it will",
+"i've" : "I have",
+"let's" : "let us",
+"mightn't" : "might not",
+"mustn't" : "must not",
+"shan't" : "shall not",
+"she'd" : "she would",
+"she'll" : "she will",
+"she's" : "she is",
+"shouldn't" : "should not",
+"that's" : "that is",
+"there's" : "there is",
+"they'd" : "they would",
+"they'll" : "they will",
+"they're" : "they are",
+"they've" : "they have",
+"we'd" : "we would",
+"we're" : "we are",
+"weren't" : "were not",
+"we've" : "we have",
+"what'll" : "what will",
+"what're" : "what are",
+"what's" : "what is",
+"what've" : "what have",
+"where's" : "where is",
+"who'd" : "who would",
+"who'll" : "who will",
+"who're" : "who are",
+"who's" : "who is",
+"who've" : "who have",
+"won't" : "will not",
+"wouldn't" : "would not",
+"you'd" : "you would",
+"you'll" : "you will",
+"you're" : "you are",
+"you've" : "you have",
+"'re": " are",
+"wasn't": "was not",
+"we'll":" will",
+"didn't": "did not",
+"tryin'":"trying"}
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
+
+def load_and_prec():
+    train_df = pd.read_csv("../input/train.csv") # 读取文件路径
+    test_df = pd.read_csv("../input/test.csv")
+    print("Train shape : ", train_df.shape)
+    print("Test shape : ", test_df.shape)
+
+    # lower
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: x.lower())
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: x.lower())
+
+    # Clean the text
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_text(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: clean_text(x))
+
+    # Clean numbers
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+
+    # Clean speelings
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+
+    ## fill up the missing values
+    train_X = train_df["question_text"].fillna("_##_").values
+    test_X = test_df["question_text"].fillna("_##_").values
+
+    ## Tokenize the sentences
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(train_X))
+    train_X = tokenizer.texts_to_sequences(train_X)
+    test_X = tokenizer.texts_to_sequences(test_X)
+
+    ## Pad the sentences
+    train_X = pad_sequences(train_X, maxlen=maxlen)
+    test_X = pad_sequences(test_X, maxlen=maxlen)
+
+    ## Get the target values
+    train_y = train_df['target'].values
+
+    # shuffling the data
+    np.random.seed(SEED)
+    trn_idx = np.random.permutation(len(train_X))
+
+    train_X = train_X[trn_idx]
+    train_y = train_y[trn_idx]
+
+    return train_X, test_X, train_y, tokenizer.word_index
